6b69d62a2c40c61569f3854b8c220752ff6ebe37
Eliana Del Mar Velásquez Rodriguez <103125856+MeerVelasquez@users.noreply.github.com>
2023-09-26 22:38:23 -0500

    Login decorado

--- lib/ui/pages/auth/login_page.dart
+++ lib/ui/pages/auth/login_page.dart
@@ -35,112 +35,135 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(20.0, 25.0, 20.0, 12.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "Login with email",
-                  style: TextStyle(fontSize: 20),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  key: const Key('TextFormFieldLoginEmail'),
-                  controller: _emailController,
-                  decoration: const InputDecoration(labelText: 'Email'),
-                  onChanged: (value) {
-                    _emailController.text = value.trim();
-                    _emailController.selection = TextSelection.fromPosition(
-                        TextPosition(offset: _emailController.text.length));
-                  },
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Enter email";
-                    } else if (!RegExp(
-                      r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
-                    ).hasMatch(value.trim())) {
-                      return "Enter valid email address";
-                    } else {
-                      return null;
-                    }
-                  },
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  key: const Key('TextFormFieldLoginPassword'),
-                  controller: _passwordController,
-                  decoration: const InputDecoration(labelText: "Password"),
-                  keyboardType: TextInputType.number,
-                  obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Enter password";
-                    } else if (value.length < 6) {
-                      return "Password should have at least 6 characters";
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                OutlinedButton(
-                    key: const Key('ButtonLoginSubmit'),
-                    onPressed: () async {
-                      // this line dismiss the keyboard by taking away the focus of the TextFormField and giving it to an unused
-                      FocusScope.of(context).requestFocus(FocusNode());
-                      final form = _formKey.currentState;
-                      form!.save();
-
-                      if (form.validate()) {
-                        try {
-                          await _authController.login(
-                              _emailController.text, _passwordController.text);
-                        } catch (e) {
-                          print(e);
-                        }
-
-                        if (_authController.logged.value) {
-                          Get.to(HomePage(
-                            key: const Key('HomePage'),
-                          ));
-                        } else {
-                          ScaffoldMessenger.of(context)
-                              .showSnackBar(const SnackBar(
-                            content: Text('User or password not ok'),
-                          ));
-                        }
-                      }
-                      // Get.to(const Quest(
-                      //   key: Key('Quest'),
-                      //   currentLevel: 1,
-                      // ));
-                    },
-                    child: const Text("Submit")),
-                const SizedBox(
-                  height: 20,
-                ),
-                TextButton(
-                    key: const Key('ButtonLoginCreateAccount'),
-                    onPressed: () => Get.to(const SignUpPage(
-                          key: Key('SignUpPage'),
-                        )),
-                    child: const Text('Create account'))
-              ],
+        resizeToAvoidBottomInset: false,
+        body: Stack(
+          children: [
+            Image.asset(
+              'assets/Seconbg.png', // Reemplaza con la ruta de tu imagen de fondo
+              width: double.infinity,
+              height: double.infinity,
+              fit: BoxFit.cover,
             ),
-          ),
-        ),
-      ),
-    );
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20.0, 25.0, 20.0, 12.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Sum+",
+                          style: TextStyle(
+                            fontFamily: 'Itim',
+                            fontSize: 64,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          )),
+                      const Text(
+                        "Login with email",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        key: const Key('TextFormFieldLoginEmail'),
+                        controller: _emailController,
+                        decoration: const InputDecoration(labelText: 'Email'),
+                        onChanged: (value) {
+                          _emailController.text = value.trim();
+                          _emailController.selection =
+                              TextSelection.fromPosition(TextPosition(
+                                  offset: _emailController.text.length));
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Enter email";
+                          } else if (!RegExp(
+                            r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+                          ).hasMatch(value.trim())) {
+                            return "Enter valid email address";
+                          } else {
+                            return null;
+                          }
+                        },
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      TextFormField(
+                        key: const Key('TextFormFieldLoginPassword'),
+                        controller: _passwordController,
+                        decoration:
+                            const InputDecoration(labelText: "Password"),
+                        keyboardType: TextInputType.number,
+                        obscureText: true,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Enter password";
+                          } else if (value.length < 6) {
+                            return "Password should have at least 6 characters";
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      OutlinedButton(
+                          key: const Key('ButtonLoginSubmit'),
+                          onPressed: () async {
+                            // this line dismiss the keyboard by taking away the focus of the TextFormField and giving it to an unused
+                            FocusScope.of(context).requestFocus(FocusNode());
+                            final form = _formKey.currentState;
+                            form!.save();
+
+                            if (form.validate()) {
+                              try {
+                                await _authController.login(
+                                    _emailController.text,
+                                    _passwordController.text);
+                              } catch (e) {
+                                print(e);
+                              }
+
+                              if (_authController.logged.value) {
+                                Get.to(HomePage(
+                                  key: const Key('HomePage'),
+                                ));
+                              } else {
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(const SnackBar(
+                                  content: Text('User or password not ok'),
+                                ));
+                              }
+                            }
+                            // Get.to(const Quest(
+                            //   key: Key('Quest'),
+                            //   currentLevel: 1,
+                            // ));
+                          },
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  const Color(0xFF997AC1))),
+                          child: const Text("Submit",
+                              style: TextStyle(color: Colors.white))),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextButton(
+                          key: const Key('ButtonLoginCreateAccount'),
+                          onPressed: () => Get.to(const SignUpPage(
+                                key: Key('SignUpPage'),
+                              )),
+                          child: const Text('Create account'))
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
 
